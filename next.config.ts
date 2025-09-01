e270f4f879a8503e8ba21d1d4efa38b073bf15f7
Leo Sammarco <leo.sammarco@booking.com>
2025-09-01 13:48:04 +0100

    switched to latest next.js version

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    async redirects() {
+        return [
+            {
+                source: '/outbound/booking-transport',
+                destination: 'https://transport.booking.com/search?endLatitude=-12.11923780&endLongitude=-77.02784300&startLatitude=-12.02277900&startLongitude=-77.10642700',
+                permanent: true,
+            },
+            {
+                source: '/outbound/booking-taxi',
+                destination: 'https://taxi.booking.com',
+                permanent: true,
+            },
+            {
+                source: '/outbound/skylab',
+                destination: 'https://www.redbulladvancedtechnologies.com/',
+                permanent: true,
+            },
+            {
+                source: '/outbound/trainline',
+                destination: 'https://www.thetrainline.com/',
+                permanent: true,
+            },
+            {
+                source: '/outbound/trainline-whitelabel',
+                destination: 'https://www.buytickets.crosscountrytrains.co.uk/',
+                permanent: true,
+            },
+        ]
+    },
 };
 
 export default nextConfig;
---
+++ src/app/components/Avatar/Avatar.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import styles from "./Avatar.module.css";
+
+export type Props = {
+    imgSrc: string;
+    imgAlt: string;
+    height: number;
+    width: number;
+}
+function Avatar({ imgSrc, imgAlt, height, width }: Props) {
+    return (
+        <div className={styles.avatar} style={{height, width, position: 'relative'}}>
+            <Image
+                src={imgSrc}
+                alt={imgAlt}
+                fill={true}
+                priority={true}
+                sizes="200px"
+                className={styles.avatarImage} />
+        </div>
+    );
+}
+
+export default Avatar;
